977987ff54fa7f4a39c9b37ee5319a83be320ba6
Chris Capisce <christof1588@gmail.com>
2022-11-02 13:31:32 +0100

    Update asn-iptables-blocklist.sh

--- asn-iptables-blocklist.sh
+++ asn-iptables-blocklist.sh
@@ -5,13 +5,13 @@
 #
 
 BLACKLISTS=(
-  "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/202425/ipv4-aggregated.txt"
-  "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/267784/ipv4-aggregated.txt"
-  "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/48721/ipv4-aggregated.txt"
-  "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/209588/ipv4-aggregated.txt"
-  "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/213010/ipv4-aggregated.txt"
-  "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/399471/ipv4-aggregated.txt"
-  "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/51447/ipv4-aggregated.txt"
+  "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/202425/ipv4-aggregated.txt" # AS202425 (INT-NETWORK) IP Volume inc
+  "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/267784/ipv4-aggregated.txt" # AS267784 (FLYSERVERS-SA) Flyservers S.A.
+  "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/48721/ipv4-aggregated.txt" # AS48721 (FLYSERVERS-ENDCLIENTS) Flyservers S.A.
+  "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/209588/ipv4-aggregated.txt" # AS209588 (FLYSERVERS) Flyservers S.A.
+  "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/213010/ipv4-aggregated.txt" # AS213010 (GIGAHOSTINGSERVICES) GigaHostingServices OU
+  "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/399471/ipv4-aggregated.txt" # AS399471 (SERVERION) Serverion LLC
+  "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/51447/ipv4-aggregated.txt" # AS51447 (ROOTLAYERNET) RootLayer Web Services Ltd.
 )
 
 IP_BLACKLIST_TMP=$(mktemp)
