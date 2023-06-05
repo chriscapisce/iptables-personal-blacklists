efa45970ffe65d25198f55af905ffdf9519894a6
Chris Capisce <christof1588@gmail.com>
2023-06-05 22:10:44 +0200

    Update local-asn-personal-blacklist.sh

--- local-asn-personal-blacklist.sh
+++ local-asn-personal-blacklist.sh
@@ -26,6 +26,7 @@ BLACKLISTS=(
         "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/12271/ipv4-aggregated.txt"  # AS12271 (TWC-NYC) Charter Communications Inc
         "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/33667/ipv4-aggregated.txt"  # AS33667 (CMCS) Comcast Cable Communications, LLC
         "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/33668/ipv4-aggregated.txt"  # AS33668 (CMCS) Comcast Cable Communications, LLC
+        "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/20214/ipv4-aggregated.txt" # AS20214 (COMCAST) Comcast Cable Communications, LLC
         "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/198896/ipv4-aggregated.txt" # AS198896 (CITYLAN) CITYLAN Sp. z o.o.
 )
 
